597e5369b9f46c73bc773fb13baf313aff22b1dc
Sebastian David Ibañez Rios <sdibanez@uninorte.edu.co>
2025-05-15 22:45:28 -0500

    part 2

---
+++ part 2/src/sqr_r_sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sqr_r_sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        a= Integer.parseInt(scanner.nextLine());
+        b= Integer.parseInt(scanner.nextLine());
+
+        System.out.println(Math.sqrt(a+b));
+
+    }
+}
---
+++ part 2/src/squared.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class squared {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = Integer.parseInt(sc.nextLine());
+        System.out.println(Math.pow(n,2) );
+    }
+}
